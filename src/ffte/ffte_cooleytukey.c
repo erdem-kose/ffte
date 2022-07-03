59202f7fbd6c56da51a4d77b2b6fe1e17d84486c
erdem <erdemkose78@gmail.com>
2022-07-03 15:22:28 +0300

    Cooley-Tukey improvements, speed enchancements

--- src/ffte/ffte_cooleytukey.c
+++ src/ffte/ffte_cooleytukey.c
@@ -6,21 +6,16 @@
 #include "subfuncs.h"
 #include "../ffte.h"
 
-static void swap(float* a, float* b)
-{
-	float temp = *a;
-	*a=*b;
-	*b= temp;
-}
+void swap(float* X, uint64_t i, uint64_t j);
 
-void ffte_cooleytukey(float x_real[], float x_imag[], size_t N, unsigned char only_real, unsigned char inverse)
+void ffte_cooleytukey(float x_real[], float x_imag[], size_t N, unsigned char only_real_input, unsigned char inverse)
 {
 	// Parameter calculation
 	double PI_N = (inverse!=0)? (-M_PI):(M_PI);
 	int64_t M = log((double)N) / log(2.0);
 
 	// If only real input enabled, fill x_imag with zeros
-	if (only_real != 0)
+	if (only_real_input != 0)
 	{
 		for (int64_t i = 0; i < N; ++i)
 		{
@@ -28,31 +23,51 @@ void ffte_cooleytukey(float x_real[], float x_imag[], size_t N, unsigned char on
 		}
 	}
 
-	// Perform bit reversal.
-	int64_t j, k;
+	// Input reordering with bit reversal 
+	int64_t i_new;
 	double arg;
 
-	for (int64_t i = 0; i < N; ++i)
+	if (only_real_input == 0)
 	{
-		j = 0;
-
-		for (k = 0; k < M; ++k)
+		for (int64_t i = 0; i < N; ++i)
 		{
-			j = (j << 1) | (1 & (i >> k));
-		}
+			i_new = 0;
 
-		if (j < i)
-		{
-			swap(&x_real[i], &x_real[j]);
-			swap(&x_imag[i], &x_imag[j]);
+			for (int64_t i_shift = 0; i_shift < M; ++i_shift)
+			{
+				i_new = (i_new << 1) | (1 & (i >> i_shift));
+			}
+
+			if (i_new < i)
+			{
+				swap(x_real, i, i_new);
+				swap(x_imag, i, i_new);
+			}
 		}
 	}
+	else
+	{
+		for (int64_t i = 0; i < N; ++i)
+		{
+			i_new = 0;
 
-	// For log N stages.
+			for (int64_t i_shift = 0; i_shift < M; ++i_shift)
+			{
+				i_new = (i_new << 1) | (1 & (i >> i_shift));
+			}
+
+			if (i_new < i)
+			{
+				swap(x_real, i, i_new);
+			}
+		}
+	}
+	
+	// log(N) stages
 	int64_t n1 = 1;
 	int64_t n2 = n1 << 1;
 
-	int64_t j1, j2;
+	int64_t k, j1, j2;
 
 	double w, s, c;
  	double r_tmp, i_tmp;
@@ -63,19 +78,22 @@ void ffte_cooleytukey(float x_real[], float x_imag[], size_t N, unsigned char on
 
 		k = 0;
 
-		// For N components.
+		// N stages
 		while (k < N - 1)
 		{
-			// For each section.
-			for (j = 0; j < n1; j++)
+			// Sections
+			for (int64_t j = 0; j < n1; j++)
 			{
 				arg = -j * w;
+
 				c = cos(arg);
 				s = sin(arg);
+
 				j1 = k + j;
 				j2 = j1 + n1;
-				r_tmp = x_real[j2] * c - x_imag[j1 + n1] * s;
-				i_tmp = x_imag[j2] * c + x_real[j2] * s;
+
+				cmplx_mul(&r_tmp, &i_tmp, x_real[j2], x_imag[j2], c , s);
+
 				x_real[j2] = x_real[j1] - r_tmp;
 				x_imag[j2] = x_imag[j1] - i_tmp;
 				x_real[j1] = x_real[j1] + r_tmp;
@@ -96,4 +114,11 @@ void ffte_cooleytukey(float x_real[], float x_imag[], size_t N, unsigned char on
 			x_imag[i] /= N;
 		}
 	}
+}
+
+inline void swap(float* X, uint64_t i, uint64_t j)
+{
+	float temp = X[i];
+	X[i]=X[j];
+	X[j]= temp;
 }
