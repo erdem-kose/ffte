d9792b5b114aa288d9b6d50d076a41c264a76178
erdem <erdemkose78@gmail.com>
2022-08-14 16:34:57 +0300

    Plotting abilities added to main.c for test purposes

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 Fast Fourier Transform for Embedded Systems in C. 
  * Power of 2 FFT's calculated by Cooley-Tukey
  * Non-power of 2 FFT's calculated by Bluestein
+ * 2D FFT supported
 
- * Will be Posix compilant. 1D and 2D supported
- 
- * Demo script is for Linux only, and you should install ```sudo apt install libplplot-dev```
+ * Demo script `main.c` requires ```sudo apt install libplplot-dev```
--- main.c
+++ main.c
@@ -1,86 +1,75 @@
 #include <math.h>
 #include <stdio.h>
+#include <plplot/plplot.h>
 #include "src/ffte.h"
 
+void plot(const char* filename, double x[], double y[], int N, const char* x_lbl, const char* y_lbl)
+{
+    PLFLT xmin = INFINITY, xmax = -INFINITY;
+    PLFLT ymin = INFINITY, ymax = -INFINITY;
 
-int main()
+    unsigned int i;
+    for(i=0; i<N; ++i)
+    {
+        xmax=(x[i]>xmax)?x[i]:xmax;
+
+        if(x[i]<xmin)
+            xmin=x[i];
+        if(y[i]>ymax)
+            ymax=y[i];
+        if(y[i]<ymin)
+            ymin=y[i];
+    }
+    // Plplot
+    plsetopt("dev","pdf");
+    plsetopt("o", filename);
+    
+    plinit();
+
+    plenv( xmin, xmax, ymin, ymax, 0, 0 );
+    pllab( x_lbl, y_lbl, "" );
+
+    plline( N, x, y );
+
+    plend();
+}
+
+void test_ffte1d()
 {
     // Create data
     unsigned int N = 128;
     double x_r[N];
     double x_i[N];
-
+    double f_axis[N];
+    double t_axis[N];
     double f=9.375;//10 for N=100 , 9.375 for N=128
     double fs=200;
 
     for(int i=0; i< N; ++i)
     {
         x_r[i]=cos(2.0*M_PI*f/fs*((double)i));
+        f_axis[i]=i*fs/N;
+        t_axis[i]=i/fs;
     }
-    
-    // Print data
-    printf("\nInput Signal:\n");
-    for(int i=0; i< N; ++i)
-    {
-        printf("\nReal: %f",x_r[i]);
-    }
-    printf("\n");
+
+    // Plot data
+    plot("../ffte_x.pdf", t_axis, x_r , N, "t", "X");
 
     // Take FFT
     ffte(x_r,x_i,N,1);
-    
-    // Print output
-    printf("\nFFT:\n");
-    for(int i=0; i< N; ++i)
-    {
-        printf("\nf: %f, Abs: %f",i*fs/N, sqrt(x_r[i]*x_r[i] + x_i[i] * x_i[i]));
-    }
-    printf("\n");
+
+    plot("../ffte_Xr.pdf", f_axis, x_r , N, "f", "X");
+    plot("../ffte_Xi.pdf", f_axis, x_i , N, "f", "X");
 
     // Take IFFT
     iffte(x_r,x_i,N,0);
-    
-    // Print output
-    printf("\nOutput Signal:\n");
-    for(int i=0; i< N; ++i)
-    {
-        printf("\nReal: %f",x_r[i]);
-    }
-    printf("\n");
+
+    plot("../ffte_x_ifft.pdf", t_axis, x_r , N, "t", "X");
+}
+
+int main(int argc, char **argv )
+{
+    test_ffte1d();
 
     return 0;
 }
-
-/*
-main( int argc, char *argv[] )
-{
-    PLFLT x[NSIZE], y[NSIZE];
-    PLFLT xmin = 0., xmax = 1., ymin = 0., ymax = 100.;
-    int   i;
-
-    // Prepare data to be plotted.
-    for ( i = 0; i < NSIZE; i++ )
-    {
-        x[i] = (PLFLT) ( i ) / (PLFLT) ( NSIZE - 1 );
-        y[i] = ymax * x[i] * x[i];
-    }
-
-    // Parse and process command line arguments
-    plparseopts( &argc, argv, PL_PARSE_FULL );
-
-    // Initialize plplot
-    plinit();
-
-    // Create a labelled box to hold the plot.
-    plenv( xmin, xmax, ymin, ymax, 0, 0 );
-    pllab( "x", "y=100 x#u2#d", "Simple PLplot demo of a 2D line plot" );
-
-    // Plot the data that was prepared above.
-    plline( NSIZE, x, y );
-
-    // Close PLplot library
-    plend();
-
-    exit( 0 );
-}
-*/
