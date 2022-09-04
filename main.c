173cba1437440b48623f9d372d4486893ee1200b
erdem <erdemkose78@gmail.com>
2022-09-04 13:35:47 +0300

    execution time added, test improved

--- main.c
+++ main.c
@@ -1,12 +1,12 @@
 #include <math.h>
 #include <stdio.h>
 #include "utils/plot.h"
+#include "utils/exectime.h"
 #include "src/ffte.h"
 
-void test_ffte1d()
+void test_ffte1d(unsigned int N)
 {
     // Create data
-    unsigned int N = 128;
     double x_r[N];
     double x_i[N];
     double f_axis[N];
@@ -21,24 +21,36 @@ void test_ffte1d()
         t_axis[i]=i/fs;
     }
 
+    // Execution time measurement variables
+    double ffte_time, iffte_time;
+
     // Plot data
     plot("../ffte_x.pdf", t_axis, x_r , N, "t", "X");
 
     // Take FFT
+    tic();
     ffte(x_r,x_i,N,1);
+    ffte_time=toc();
+    printf("\tFFTE takes %f secs \r\n", ffte_time);
 
     plot("../ffte_Xr.pdf", f_axis, x_r , N, "f", "X");
     plot("../ffte_Xi.pdf", f_axis, x_i , N, "f", "X");
 
     // Take IFFT
+    tic();
     iffte(x_r,x_i,N,0);
+    iffte_time=toc();
+    printf("\tIFFTE takes %f secs \r\n", iffte_time);
 
     plot("../ffte_x_ifft.pdf", t_axis, x_r , N, "t", "X");
 }
 
 int main(int argc, char **argv )
 {
-    test_ffte1d();
+    printf("Power of 2 FFT test\r\n");
+    test_ffte1d(1024*16);
+    printf("\n\rNon-Power of 2 FFT test\r\n");
+    test_ffte1d(1025*16); // TODO: 1024*63 gives segfault, look at that
 
     return 0;
 }
